bf98c806e4b69ac99d8dad5c15db255ed53c0668
Jazmín Gil <124318350+JazGil@users.noreply.github.com>
2026-05-19 23:57:05 +0000

    Cambios

--- proyecto_final/app/page.tsx
+++ proyecto_final/app/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 import Login from "./(auth)/login/login"
 import Mapa from "./Mapa/mapa";
 import Menu from "./Componentes/Menu/Menu";
-import NotFound from "./NotFound/NotFound";
+
 
 
 export default function Home() {
   return (
     
-    <NotFound/>
+    <></>
 
 
   )
