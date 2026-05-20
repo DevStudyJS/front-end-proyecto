f420ff0ff22f46881ec7aa1286143a2fb77fc9e0
Sin_R0str0 <154567676+SinR0str0@users.noreply.github.com>
2026-05-20 00:54:13 +0000

    Archivo nuevo

--- proyecto_final/app/not-found.tsx
+++ proyecto_final/app/not-found.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './NotFound.module.css';
+import styles from './not-found.module.css';
 
 
 export default function NotFound() {
